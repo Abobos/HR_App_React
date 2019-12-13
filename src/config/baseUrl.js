d95ed1bd338ea36a72b8276e0dbc9fea30ec94df
Blessing Abobo <blessingmakaraba@gmail.com>
2019-12-13 06:24:09 +0100

    Fix Bugs

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,7 +1,7 @@
 let baseUrl;
 
 process.env.NODE_ENV === 'production'
-  ? (baseUrl = 'https://hr-app1.herokuapp.com')
+  ? (baseUrl = 'https://hr-app3.herokuapp.com')
   : (baseUrl = 'http://localhost:3000');
 
 export default baseUrl;
